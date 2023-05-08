f567ef67546f93260cc8afddb40e4d96e30891d1
Novi Amanda Puspita Sari <apsnovi@gmail.com>
2023-05-08 17:20:46 +0700

    branch production

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  HOST: "34.128.90.124",
-  USER: "root",
-  PASSWORD: "password",
-  DB: "db_cars",
+  HOST: "35.247.181.16",
+  USER: "dev-novi",
+  PASSWORD: "7@3|NJVjc;7;r'p%@eR*9jgc`z9xQxR",
+  DB: "db-cars",
   dialect: "mysql",
   pool: {
     max: 5,
